f164a67483e66d70800ac8c811394a695941df3d
Bryce <bhyer06@Gmail.com>
2022-10-11 07:40:51 -0600

    Ignored files/started my project

---
+++ Information/InformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  InformationApp.swift
+//  Information
+//
+//  Created by Hyer, Bryce on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct InformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
