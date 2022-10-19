a7ec147f9de0aababdfd0a13389c7326db0595d9
Bryce <bhyer06@Gmail.com>
2022-10-19 09:41:59 -0600

    Added out simplified JSON file and created variables for the items

---
+++ Information/Model/VideoGames.swift
@@ -0,0 +1,25 @@
+//
+//  VideoGames.swift
+//  Information
+//
+//  Created by Hyer, Bryce on 10/19/22.
+//
+
+import Foundation
+
+
+
+struct VideoGames
+{
+    var title : String
+    var genre : String
+    var publisher : String
+    var console : String
+    var rating : String
+    var releaseYear : Int
+    var averagePlayLength : Int
+    var leisurePlayLength : Int
+    var mediangameLength : Int
+    var rushedGameLength : Int
+}
+
