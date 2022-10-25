66c733194a8f36802eb2f7b3a4646fdbd6608dbb
Bryce <bhyer06@Gmail.com>
2022-10-25 08:54:31 -0600

    Added details for Video Game detail View

--- Information/Resources/Data/InformationView/VideoGamesDetailView.swift
+++ Information/Resources/Data/InformationView/VideoGamesDetailView.swift
@@ -9,26 +9,43 @@ import SwiftUI
 
 struct VideoGamesDetailView: View
 {
-//    var detailedVideoGame : VideoGames
+    var detailedVideoGame : VideoGames
     
     var body: some View
     {
-        HStack
+        VStack
         {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            VStack
+            Text("Video Game Item Detailed")
+                .accessibilityLabel("Detailed Video Game Title")
+                .accessibilityValue("Video game Item Details")
+                .padding()
+            HStack
             {
-                Text("Place Holder")
-                Text("Place Holder")
-                Text("Place Holder")
-                Text("Place Holder")
-            }
+                Text("Title:\n\(detailedVideoGame.title)")
+                    .accessibilityLabel("Detailed Video Game Title")
+                    .accessibilityValue("Title:\n\(detailedVideoGame.title)")
+                VStack
+                {
+                    Text("Genre: \(detailedVideoGame.genre)")
+                        .accessibilityLabel("Detailed Video Game Genre")
+                        .accessibilityValue("Genre: \(detailedVideoGame.genre)")
+                    Text("Publisher: \(detailedVideoGame.publisher)")
+                        .accessibilityLabel("Detailed Video Game Publisher")
+                        .accessibilityValue("Publisher: \(detailedVideoGame.publisher)")
+                    Text("Console: \(detailedVideoGame.console)")
+                        .accessibilityLabel("Detailed Video Game Console")
+                        .accessibilityValue("Console: \(detailedVideoGame.console)")
+                    Text("Rating: \(detailedVideoGame.rating)")
+                        .accessibilityLabel("Detailed Video Game Rating")
+                        .accessibilityValue("Rating: \(detailedVideoGame.rating)")
+                }
+        }
         }
     }
 }
 
 struct VideoGamesDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        VideoGamesDetailView()
+        VideoGamesDetailView(detailedVideoGame: demoVideoGames)
     }
 }
